13a02a876b7f978725c675324e329dd269bf4173
gaoyx <melo@gaoyuexiang.cn>
2017-05-09 17:15:57 +0800

    add exceptions

---
+++ src/main/java/cn/gaoyuexiang/LostAndFound/item/exception/LostAndFoundException.java
@@ -0,0 +1,11 @@
+package cn.gaoyuexiang.LostAndFound.item.exception;
+
+public class LostAndFoundException extends RuntimeException{
+  public LostAndFoundException() {
+    super();
+  }
+
+  public LostAndFoundException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/cn/gaoyuexiang/LostAndFound/item/exception/MissPropertyException.java
@@ -0,0 +1,11 @@
+package cn.gaoyuexiang.LostAndFound.item.exception;
+
+public class MissPropertyException extends LostAndFoundException{
+  public MissPropertyException() {
+    super();
+  }
+
+  public MissPropertyException(String message) {
+    super(message);
+  }
+}
