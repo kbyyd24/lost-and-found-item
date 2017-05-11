4da8f2c67316a82eb127ee12563f3ab421e49bcf
gaoyx <melo@gaoyuexiang.cn>
2017-05-11 17:26:45 +0800

    delete no param constructor

--- src/main/java/cn/gaoyuexiang/LostAndFound/item/exception/CloseItemException.java
+++ src/main/java/cn/gaoyuexiang/LostAndFound/item/exception/CloseItemException.java
@@ -1,14 +1,6 @@
 package cn.gaoyuexiang.LostAndFound.item.exception;
 
-import javax.ws.rs.core.Response;
-
-import static javax.ws.rs.core.Response.Status.NOT_FOUND;
-
 public class CloseItemException extends LostAndFoundException{
-  public CloseItemException() {
-    super(NOT_FOUND);
-  }
-
   public CloseItemException(String message) {
     super(message);
   }
