c14e7f8fa3ae5a4ecabdc3cfcff73879a6e3ee6c
gaoyx <melo@gaoyuexiang.cn>
2017-05-11 16:33:29 +0800

    add equals and hashCode methods in LostItemCreator

--- src/main/java/cn/gaoyuexiang/LostAndFound/item/model/dto/LostItemCreator.java
+++ src/main/java/cn/gaoyuexiang/LostAndFound/item/model/dto/LostItemCreator.java
@@ -27,6 +27,33 @@ public class LostItemCreator {
     this.pictures = pictures;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    LostItemCreator that = (LostItemCreator) o;
+
+    if (getBeginTime() != that.getBeginTime()) return false;
+    if (getEndTime() != that.getEndTime()) return false;
+    if (getTitle() != null ? !getTitle().equals(that.getTitle()) : that.getTitle() != null) return false;
+    if (getItemName() != null ? !getItemName().equals(that.getItemName()) : that.getItemName() != null) return false;
+    if (getDescription() != null ? !getDescription().equals(that.getDescription()) : that.getDescription() != null)
+      return false;
+    return getPictures() != null ? getPictures().equals(that.getPictures()) : that.getPictures() == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = getTitle() != null ? getTitle().hashCode() : 0;
+    result = 31 * result + (getItemName() != null ? getItemName().hashCode() : 0);
+    result = 31 * result + (int) (getBeginTime() ^ (getBeginTime() >>> 32));
+    result = 31 * result + (int) (getEndTime() ^ (getEndTime() >>> 32));
+    result = 31 * result + (getDescription() != null ? getDescription().hashCode() : 0);
+    result = 31 * result + (getPictures() != null ? getPictures().hashCode() : 0);
+    return result;
+  }
+
   public String getTitle() {
     return title;
   }
