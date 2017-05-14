85e9bdfe492a13638ba0bc97f3070c551a96952e
gaoyx <melo@gaoyuexiang.cn>
2017-05-14 16:26:09 +0800

    finish api get /item/found

--- src/main/java/cn/gaoyuexiang/LostAndFound/item/resource/FoundItemResource.java
+++ src/main/java/cn/gaoyuexiang/LostAndFound/item/resource/FoundItemResource.java
@@ -1,8 +1,11 @@
 package cn.gaoyuexiang.LostAndFound.item.resource;
 
+import cn.gaoyuexiang.LostAndFound.item.enums.ItemSort;
+import cn.gaoyuexiang.LostAndFound.item.enums.NotFoundReason;
 import cn.gaoyuexiang.LostAndFound.item.enums.UserState;
 import cn.gaoyuexiang.LostAndFound.item.exception.UnauthorizedException;
 import cn.gaoyuexiang.LostAndFound.item.model.dto.FoundItemCreator;
+import cn.gaoyuexiang.LostAndFound.item.model.dto.FoundItemPageItem;
 import cn.gaoyuexiang.LostAndFound.item.model.entity.FoundItem;
 import cn.gaoyuexiang.LostAndFound.item.service.FoundItemService;
 import cn.gaoyuexiang.LostAndFound.item.service.UserService;
@@ -10,6 +13,9 @@ import org.springframework.stereotype.Component;
 
 import javax.ws.rs.*;
 
+import java.util.List;
+
+import static cn.gaoyuexiang.LostAndFound.item.enums.NotFoundReason.PAGE_OUT_OF_BOUND;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Component
@@ -24,6 +30,18 @@ public class FoundItemResource {
                            UserService userService) {
     this.foundItemService = foundItemService;
     this.userService = userService;
+  }
+
+  @GET
+  public List<FoundItemPageItem> loadPage(@QueryParam("page") @DefaultValue("1") int page,
+                                          @QueryParam("listSize") @DefaultValue("8") int size,
+                                          @QueryParam("sort") @DefaultValue("create_time") String sortName) {
+    ItemSort sort = ItemSort.getItemSortByColumnName(sortName);
+    List<FoundItemPageItem> foundItemPageItems = foundItemService.loadPage(page, size, sort);
+    if (foundItemPageItems.isEmpty()) {
+      throw new NotFoundException(PAGE_OUT_OF_BOUND.getReason());
+    }
+    return foundItemPageItems;
   }
 
   @POST
---
+++ src/test/java/cn/gaoyuexiang/LostAndFound/item/resource/FoundItemResourceTestGetList.java
@@ -0,0 +1,68 @@
+package cn.gaoyuexiang.LostAndFound.item.resource;
+
+import cn.gaoyuexiang.LostAndFound.item.enums.ItemSort;
+import cn.gaoyuexiang.LostAndFound.item.model.dto.FoundItemPageItem;
+import cn.gaoyuexiang.LostAndFound.item.model.dto.Message;
+import cn.gaoyuexiang.LostAndFound.item.service.FoundItemService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.eq;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class FoundItemResourceTestGetList {
+
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  @MockBean
+  private FoundItemService foundItemService;
+
+  private int page;
+  private int size;
+  private String sortColumn;
+  private String path;
+  private String query;
+
+  @Before
+  public void setUp() throws Exception {
+    page = 1;
+    size = 8;
+    sortColumn = "create_time";
+    path = "/item/found";
+    query = String.format("?page=%d&listSize=%d&sort=%s", page, size, sortColumn);
+  }
+
+  @Test
+  public void should_response_200_when_load_page_success() throws Exception {
+    given(foundItemService.loadPage(eq(page), eq(size), eq(ItemSort.CREATE_TIME)))
+        .willReturn(Collections.singletonList(new FoundItemPageItem()));
+    ResponseEntity<List> entity = restTemplate.getForEntity(path, List.class);
+    assertThat(entity.getStatusCode(), is(HttpStatus.OK));
+  }
+
+  @Test
+  public void should_response_404_when_page_out_of_bound() throws Exception {
+    given(foundItemService.loadPage(eq(page), eq(size), eq(ItemSort.CREATE_TIME)))
+        .willReturn(Collections.emptyList());
+    ResponseEntity<Message> entity = restTemplate.getForEntity(path, Message.class);
+    assertThat(entity.getStatusCode(), is(HttpStatus.NOT_FOUND));
+  }
+}
